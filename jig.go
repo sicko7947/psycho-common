056616440ec3e5bd265289036c692a341876b359
sicko7947 <cai139193541@gmail.com>
2021-06-08 15:41:18 +1000

    minor change

--- jig.go
+++ jig.go
@@ -7,17 +7,17 @@ import (
 	"github.com/brianvoe/gofakeit/v6"
 )
 
-func Jig(src string) (dst string) {
+func Jig(src string) string {
 	faker := gofakeit.New(0)
 
 	charcount := strings.Count(src, "#")
 	numcount := strings.Count(src, "$")
 
 	for i := 0; i < charcount; i++ {
-		dst = strings.Replace(src, "#", faker.LetterN(1), 1)
+		src = strings.Replace(src, "#", faker.LetterN(1), 1)
 	}
 	for i := 0; i < numcount; i++ {
-		dst = strings.Replace(dst, "$", fmt.Sprint(faker.Number(0, 9)), 1)
+		src = strings.Replace(src, "$", fmt.Sprint(faker.Number(0, 9)), 1)
 	}
-	return dst
+	return src
 }
