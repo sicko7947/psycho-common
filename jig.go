df364b2daf35fba25d39142d4ba6ea35e179c1f6
sicko7947 <cai139193541@gmail.com>
2021-06-08 15:35:33 +1000

    minor change

--- jig.go
+++ jig.go
@@ -2,13 +2,22 @@ package sickocommon
 
 import (
 	"fmt"
-	"github.com/brianvoe/gofakeit/v6"
 	"strings"
+
+	"github.com/brianvoe/gofakeit/v6"
 )
 
 func Jig(src string) (dst string) {
 	faker := gofakeit.New(0)
-	dst = strings.ReplaceAll(src, `#`, faker.LetterN(1))
-	dst = strings.ReplaceAll(dst, `$`, fmt.Sprint(faker.Number(0, 9)))
+
+	charcount := strings.Count(src, "#")
+	numcount := strings.Count(src, "$")
+
+	for i := 0; i < charcount; i++ {
+		dst = strings.Replace(src, "#", faker.LetterN(1), 1)
+	}
+	for i := 0; i < numcount; i++ {
+		dst = strings.Replace(dst, "$", fmt.Sprint(faker.Number(0, 9)), 1)
+	}
 	return dst
 }
